fd4953266357a080c4fd2a0e0ea95a579e9ac903
windyaan <windyanurmaretha@gmail.com>
2024-11-21 18:30:45 +0700

    navController utk mengatur navigasi

--- app/src/main/java/com/example/project/Navigasi.kt
+++ app/src/main/java/com/example/project/Navigasi.kt
@@ -1,6 +1,22 @@
 package com.example.project
 
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+import com.example.project.ui.viewmodel.MahasiswaViewModel
+
 enum class Halaman {
     Form,
     Data
+}
+
+@Composable
+fun Navigasi(
+    modifier: Modifier = Modifier,
+    viewModel: MahasiswaViewModel = viewModel(),
+    navHost: NavHostController = rememberNavController()
+) {
+
 }
